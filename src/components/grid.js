9e9e5b24a370782f24d300be1d898bf32530f229
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2022-10-23 00:47:22 -0400

    Be able to increment counter on clicking each box

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,34 +1,42 @@
 import styles from "../styles/grid.css";
+import { useState } from "react";
 
 const Grid = () => {
-  const handleClick = (e) => {
+  const [Count, setCount] = useState(0);
+  const [MemoryData, setMemoryData] = useState([]);
+
+  const HandleClick = (e) => {
     console.log(e.currentTarget.dataset.value);
+    setCount(Count + 1);
   };
   return (
-    <div className="GridContainer">
-      <div className="Box" data-value="1" onClick={handleClick}>
-        1
-      </div>
-      <div className="Box" data-value="2" onClick={handleClick}>
-        2
-      </div>
-      <div className="Box" data-value="3" onClick={handleClick}>
-        3
-      </div>
-      <div className="Box" data-value="4" onClick={handleClick}>
-        4
-      </div>
-      <div className="Box" data-value="5" onClick={handleClick}>
-        5
-      </div>
-      <div className="Box" data-value="6" onClick={handleClick}>
-        6
-      </div>
-      <div className="Box" data-value="7" onClick={handleClick}>
-        7
-      </div>
-      <div className="Box" data-value="8" onClick={handleClick}>
-        8
+    <div className="Container">
+      <div className="counter">Score: {Count}</div>
+      <div className="GridContainer">
+        <div className="Box" data-value="1" onClick={HandleClick}>
+          1
+        </div>
+        <div className="Box" data-value="2" onClick={HandleClick}>
+          2
+        </div>
+        <div className="Box" data-value="3" onClick={HandleClick}>
+          3
+        </div>
+        <div className="Box" data-value="4" onClick={HandleClick}>
+          4
+        </div>
+        <div className="Box" data-value="5" onClick={HandleClick}>
+          5
+        </div>
+        <div className="Box" data-value="6" onClick={HandleClick}>
+          6
+        </div>
+        <div className="Box" data-value="7" onClick={HandleClick}>
+          7
+        </div>
+        <div className="Box" data-value="8" onClick={HandleClick}>
+          8
+        </div>
       </div>
     </div>
   );
