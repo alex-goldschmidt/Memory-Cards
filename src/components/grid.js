695ae4b744cc48b1aa1b90333a3c843767bc4a94
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2022-10-26 00:05:33 -0400

    Display best score from previous game played

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,8 +1,9 @@
 import styles from "../styles/grid.css";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const Grid = () => {
   let [Count, setCount] = useState(0);
+  const BestScore = useRef(0);
   let [Box1Count, setBox1Count] = useState(0);
   let [Box2Count, setBox2Count] = useState(0);
   let [Box3Count, setBox3Count] = useState(0);
@@ -14,89 +15,121 @@ const Grid = () => {
 
   const HandleBox1Click = (e) => {
     const target = e.currentTarget;
+    const GridContainer = document.querySelector(".GridContainer");
+    const ResetGame = document.querySelector(".ResetGame");
     setCount((Count += 1));
     setBox1Count((Box1Count += 1));
     if (target.classList.contains("Box1") && Box1Count >= 2 && Count >= 2) {
       setBox1Count((Box1Count = 0));
       setCount((Count = 0));
       console.log("this has been clicked twice");
+      GridContainer.style.display = "none";
+      ResetGame.style.display = "flex";
     }
   };
 
   const HandleBox2Click = (e) => {
     const target = e.currentTarget;
+    const GridContainer = document.querySelector(".GridContainer");
+    const ResetGame = document.querySelector(".ResetGame");
     setCount((Count += 1));
     setBox2Count((Box2Count += 1));
     if (target.classList.contains("Box2") && Box2Count >= 2 && Count >= 2) {
       setBox2Count((Box2Count = 0));
       setCount((Count = 0));
       console.log("this has been clicked twice");
+      GridContainer.style.display = "none";
+      ResetGame.style.display = "flex";
     }
   };
 
   const HandleBox3Click = (e) => {
     const target = e.currentTarget;
+    const GridContainer = document.querySelector(".GridContainer");
+    const ResetGame = document.querySelector(".ResetGame");
     setCount((Count += 1));
     setBox3Count((Box3Count += 1));
     if (target.classList.contains("Box3") && Box3Count >= 2 && Count >= 2) {
       setBox3Count((Box3Count = 0));
       setCount((Count = 0));
       console.log("this has been clicked twice");
+      GridContainer.style.display = "none";
+      ResetGame.style.display = "flex";
     }
   };
 
   const HandleBox4Click = (e) => {
     const target = e.currentTarget;
+    const GridContainer = document.querySelector(".GridContainer");
+    const ResetGame = document.querySelector(".ResetGame");
     setCount((Count += 1));
     setBox4Count((Box4Count += 1));
     if (target.classList.contains("Box4") && Box4Count >= 2 && Count >= 2) {
       setBox3Count((Box4Count = 0));
       setCount((Count = 0));
       console.log("this has been clicked twice");
+      GridContainer.style.display = "none";
+      ResetGame.style.display = "flex";
     }
   };
 
   const HandleBox5Click = (e) => {
     const target = e.currentTarget;
+    const GridContainer = document.querySelector(".GridContainer");
+    const ResetGame = document.querySelector(".ResetGame");
     setCount((Count += 1));
     setBox5Count((Box5Count += 1));
     if (target.classList.contains("Box5") && Box5Count >= 2 && Count >= 2) {
       setBox5Count((Box5Count = 0));
       setCount((Count = 0));
       console.log("this has been clicked twice");
+      GridContainer.style.display = "none";
+      ResetGame.style.display = "flex";
     }
   };
 
   const HandleBox6Click = (e) => {
     const target = e.currentTarget;
+    const GridContainer = document.querySelector(".GridContainer");
+    const ResetGame = document.querySelector(".ResetGame");
     setCount((Count += 1));
     setBox6Count((Box6Count += 1));
     if (target.classList.contains("Box6") && Box6Count >= 2 && Count >= 2) {
       setBox6Count((Box6Count = 0));
       setCount((Count = 0));
       console.log("this has been clicked twice");
+      GridContainer.style.display = "none";
+      ResetGame.style.display = "flex";
     }
   };
 
   const HandleBox7Click = (e) => {
     const target = e.currentTarget;
+    const GridContainer = document.querySelector(".GridContainer");
+    const ResetGame = document.querySelector(".ResetGame");
     setCount((Count += 1));
     setBox7Count((Box7Count += 1));
     if (target.classList.contains("Box7") && Box7Count >= 2 && Count >= 2) {
       setBox7Count((Box7Count = 0));
       setCount((Count = 0));
       console.log("this has been clicked twice");
+      GridContainer.style.display = "none";
+      ResetGame.style.display = "flex";
     }
   };
 
   const HandleBox8Click = (e) => {
     const target = e.currentTarget;
+    const GridContainer = document.querySelector(".GridContainer");
+    const ResetGame = document.querySelector(".ResetGame");
     setCount((Count += 1));
     setBox8Count((Box8Count += 1));
     if (target.classList.contains("Box8") && Box8Count >= 2 && Count >= 2) {
       setBox8Count((Box8Count = 0));
       setCount((Count = 0));
       console.log("this has been clicked twice");
+      GridContainer.style.display = "none";
+      ResetGame.style.display = "flex";
     }
   };
 
@@ -113,9 +146,28 @@ const Grid = () => {
     }
   };
 
+  useEffect(() => {
+    if (
+      HandleBox1Click ||
+      HandleBox2Click ||
+      HandleBox3Click ||
+      HandleBox4Click ||
+      HandleBox5Click ||
+      HandleBox6Click ||
+      HandleBox7Click ||
+      HandleBox8Click
+    ) {
+      BestScore.current = Count;
+    }
+  }, [Count]);
+
   return (
     <div className="Container">
       <div className="counter">Score: {Count}</div>
+      <div className="ResetGame">
+        Best Score: {BestScore.current}
+        <button className="ResetButton">Reset Game?</button>
+      </div>
       <div className="GridContainer" onClick={RandomizeBoxOrder}>
         <div className="Box1" data-value="1" onClick={HandleBox1Click}>
           1
